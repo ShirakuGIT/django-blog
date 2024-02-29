16df57b6643da9d7383aaa0a31397b46bc10a8fe
ShirakuGIT <shiraku.official@gmail.com>
2024-02-29 11:51:31 +0530

    changed debug settings

--- bloggy/bloggy/settings.py
+++ bloggy/bloggy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ohz(kii0+1$*yh(-pk-&02!qy615l-(31%=**v-hwkjrhmhehi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
